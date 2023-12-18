ec909c3423f04d8c488d7740962ebf8ee0bf60ee
Mani Movassagh <mani.mgh@gmail.com>
2023-12-18 17:43:48 +0100

    define telescope fuzzy finder

--- init.lua
+++ init.lua
@@ -16,9 +16,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+{
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
