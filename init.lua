f557ce082638c163b39ef3132d5f9c5d1496c1c5
Mani Movassagh <mani.mgh@gmail.com>
2023-12-18 18:03:52 +0100

    define telescope options

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -27,4 +28,6 @@ require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
