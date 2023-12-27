20aa1ada30d67682d978b13d529aa19a8a2dfe29
Mani Movassagh <mani.mgh@gmail.com>
2023-12-27 21:30:22 +0100

    define auto formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,9 +3,11 @@ return {
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
-            source = {null_ls.builtins.formatting.stylua, null_ls.builtins.diagnostics.eslint,
-                      null_ls.builtins.completion.spell, null_ls.builtins.code_actions.xo,
-                      null_ls.builtins.diagnostics.flake8}
+            source = {null_ls.builtins.formatting.stylua,#
+             null_ls.builtins.formatting.prettier,
+             null_ls.builtins.formatting.isort,
+             null_ls.builtins.formatting.black,
+                      null_ls.builtins.diagnostics.eslint_d}
         })
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
 
