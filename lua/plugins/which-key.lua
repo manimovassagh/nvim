5168ac70c712b0c8660728afabd0a72e1491ce26
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2024-03-15 02:30:47 +0100

    Update which-key.lua

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,1 +1,18 @@
 
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        vim.api.nvim_set_keymap("n", "<leader>f",  "format code", { noremap = true })
+        
+    end,
+    opts = { 
+
+        
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
