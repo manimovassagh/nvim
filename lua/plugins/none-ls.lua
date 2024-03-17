fa5f0fadeab7f96d766dc4c6dd8805ae9787844a
Mani Movassagh <mani.mgh@gmail.com>
2024-03-17 18:24:49 +0100

    debug formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,11 +3,10 @@ return {
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
-            source = {null_ls.builtins.formatting.stylua,#
-             null_ls.builtins.formatting.prettier,
-             null_ls.builtins.formatting.isort,
-             null_ls.builtins.formatting.black,
-                      null_ls.builtins.diagnostics.eslint_d}
+            source = {null_ls.builtins.formatting.stylua, null_ls.builtins.formatting.prettier,
+                      null_ls.builtins.formatting.isort, null_ls.builtins.formatting.black,
+                      --null_ls.builtins.diagnostics.eslint_d
+                    }
         })
         vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, {})
 
